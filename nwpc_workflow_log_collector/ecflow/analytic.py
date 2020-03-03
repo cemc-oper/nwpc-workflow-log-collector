ec3820f7159d23c897d02f03124c7bea0d166b1b
perillaroc <perillaroc@gmail.com>
2020-03-03 18:09:40 +0800

    :recycle: split into multi functions.

--- nwpc_workflow_log_collector/ecflow/analytic.py
+++ nwpc_workflow_log_collector/ecflow/analytic.py
@@ -3,7 +3,7 @@ import re
 from itertools import islice
 
 from loguru import logger
-from nwpc_workflow_log_model.log_record.ecflow import EcflowLogRecord, EcflowLogParser, StatusLogRecord
+from nwpc_workflow_log_model.log_record.ecflow import EcflowLogParser, StatusLogRecord
 from nwpc_workflow_model.node_status import NodeStatus
 import pyprind
 import pandas as pd
@@ -15,18 +15,32 @@ from .log_file_util import get_line_no_range
 def analytics_node_log_with_status(
         file_path: str,
         node_path: str,
-        node_status: NodeStatus.submitted,
+        node_status: NodeStatus,
         start_date: datetime.datetime,
         end_date: datetime.datetime,
         verbose: int,
 ):
-    with open(file_path) as f:
-        logger.info(f"Analytic time points")
-        logger.info(f"\tnode_path: {node_path}")
-        logger.info(f"\tnode_status: {node_status}")
-        logger.info(f"\tstart_date: {start_date}")
-        logger.info(f"\tend_date: {end_date}")
+    logger.info(f"Analytic time points")
+    logger.info(f"\tnode_path: {node_path}")
+    logger.info(f"\tnode_status: {node_status}")
+    logger.info(f"\tstart_date: {start_date}")
+    logger.info(f"\tend_date: {end_date}")
 
+    logger.info(f"Getting log lines...")
+    records = get_record_list(file_path, node_path, start_date, end_date)
+    logger.info(f"Getting log lines...Done, {len(records)} lines")
+
+    analytic_status_point(records, node_path, node_status, start_date, end_date)
+
+
+def get_record_list(
+        file_path: str,
+        node_path: str,
+        start_date: datetime.datetime,
+        end_date: datetime.datetime,
+):
+    records = []
+    with open(file_path) as f:
         logger.info(f"Finding line range in date range: {start_date}, {end_date}")
         begin_line_no, end_line_no = get_line_no_range(
             file_path,
@@ -52,8 +66,6 @@ def analytics_node_log_with_status(
             next(f)
             pbar_before.update()
 
-        record_list = []
-
         prog = re.compile(f"{node_path}")
 
         logger.info(f"Reading lines between {begin_line_no} and {end_line_no}")
@@ -69,21 +81,35 @@ def analytics_node_log_with_status(
 
             parser = EcflowLogParser()
             record = parser.parse(line)
-            if record.node_path == node_path and isinstance(record, StatusLogRecord):
-                if record.status == node_status:
-                    record_list.append(record)
+            records.append(record)
 
-        print(f"Time series for {node_path} with {node_status}:")
-        for r in record_list:
-            print(f"{r.date} {r.time}")
+    return records
 
-        time_series = pd.Series([
-            (datetime.datetime.combine(r.date, r.time) - datetime.datetime.combine(r.date, datetime.time.min))
-            for r in record_list
-        ])
-        print("Mean:")
-        print(time_series.mean())
 
-        trim_mean = stats.trim_mean(time_series.values, 0.25)
-        print("Trim Mean (0.25):")
-        print(pd.to_timedelta(trim_mean))
+def analytic_status_point(
+        records: list,
+        node_path: str,
+        node_status: NodeStatus,
+        start_date: datetime.datetime,
+        end_date: datetime.datetime,
+):
+    record_list = []
+    for record in records:
+        if record.node_path == node_path and isinstance(record, StatusLogRecord):
+            if record.status == node_status:
+                record_list.append(record)
+
+    print(f"Time series for {node_path} with {node_status}:")
+    for r in record_list:
+        print(f"{r.date} {r.time}")
+
+    time_series = pd.Series([
+        (datetime.datetime.combine(r.date, r.time) - datetime.datetime.combine(r.date, datetime.time.min))
+        for r in record_list
+    ])
+    print("Mean:")
+    print(time_series.mean())
+
+    trim_mean = stats.trim_mean(time_series.values, 0.25)
+    print("Trim Mean (0.25):")
+    print(pd.to_timedelta(trim_mean))
